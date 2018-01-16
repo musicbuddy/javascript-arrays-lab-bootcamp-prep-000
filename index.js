3fbd063e38d841ba3264bd0256ade2d03e77e9f5
Ociel Coria-Jimenez <oc102892@gmail.com>
2018-01-16 03:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAPpendKitten(name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
